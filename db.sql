6bf564907a281383572c01915e9ddd93ae212a60
shane-m-calhoun <shane.m.calhoun@gmail.com>
2017-04-13 15:17:35 -0600

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pictures
+(
+    [location_id] integer PRIMARY KEY NOT NULL,
+    [img1] text NOT NULL,
+    [img2] text,
+    [img3] text,
+    [img4] text,
+    [img5] text,
+    [img6] text
+);
--- main.py
+++ main.py
@@ -16,11 +16,20 @@ def addLocations():
     location_id = request.args.get('location_id')
     location_name = request.args.get('location_name')
     location_description = request.args.get('location_description')
+    img1 = request.args.get('img1')
+    img2 = request.args.get('img2')
+    img3 = request.args.get('img3')
+    img4 = request.args.get('img4')
+    img5 = request.args.get('img5')
+    img6 = request.args.get('img6')
     db = connect_db()
     sql = "insert into locations (location_id, location_name, location_description) values (?, ?, ?)"
+    sql1 = "insert into pictures (location_id, img1, img2, img3, img4, img5, img6) values (?, ?, ?, ?, ?, ?, ?)"
     db.execute(sql, [location_id, location_name, location_description])
+    db.execute(sql1, [location_id, img1, img2, img3, img4, img5, img6])
     db.commit()
     cur = db.execute("select * from locations")
+    cur = db.execute("select * from pictures")
     entries = [dict(location_id=row[0], location_name=row[1], location_description=row[2]) for row in cur.fetchall()]
     db.close()
     return render_template('LocationList.html', entries=entries)
