ce5f8531d30e5e66df413074523565924f067a91
Daniel Frentzel <danielfrentzel@gmail.com>
2018-07-07 18:52:43 -0600

    Removed db.sql so heroku can execute init_db.py

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE locations
-(
-    [location_id] integer PRIMARY KEY AUTOINCREMENT,
-    [location_name] text NOT NULL,
-    [location_description] text NOT NULL,
-    [location_x] text NOT NULL,
-    [location_y] text NOT NULL
-);
