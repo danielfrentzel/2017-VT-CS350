2c05a5a6abfe0a8fca82fce3743995910ad3d931
Daniel Frentzel <danielfrentzel@gmail.com>
2018-07-07 19:57:40 -0600

    updated heroku files and migrations folder was added

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
@@ -8,6 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 class locations(db.Model):
 
@@ -16,3 +19,6 @@ class locations(db.Model):
     location_description = db.Column(db.String(128))
     location_x = db.Column(db.String(128))
     location_x = db.Column(db.String(128))
+
+if __name__ == '__main__':
+    manager.run()
