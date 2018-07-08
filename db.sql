ec9589b8d3c6bf3e5639411d0a0911e71395bf55
Daniel Frentzel <danielfrentzel@gmail.com>
2018-07-07 19:00:01 -0600

    Removed myapp.db and replaced db.sql

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE locations
+(
+    [location_id] integer PRIMARY KEY AUTOINCREMENT,
+    [location_name] text NOT NULL,
+    [location_description] text NOT NULL,
+    [location_x] text NOT NULL,
+    [location_y] text NOT NULL
+);
